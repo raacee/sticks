b3dff3ab817ab844765fb579a3c5adae24f2eaaf
racel <raceldamechli@outlook.fr>
2023-04-16 16:46:30 +0300

    added styling and elements to loader

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,5 +1,5 @@
 <template>
-
+  <div class="loader"></div>
 </template>
 
 <script>
@@ -9,5 +9,17 @@ export default {
 </script>
 
 <style scoped>
+.loader {
+    border: 4px solid #70422a;
+    border-top: 4px solid #d57c11;
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    animation: spin 2s linear infinite;
+}
 
+@keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+}
 </style>
